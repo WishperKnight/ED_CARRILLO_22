a9087d933560444a235b288e32c9b26fe3e17156
WishperKnight <116265076+WishperKnight@users.noreply.github.com>
2022-12-01 02:26:04 +0100

    Maven PDF
    Librerias para anidar proyectos java y maven

---
+++ fibonacci/src/main/java/ies/carrillo/daw/endes/fibonacci/App.java
@@ -0,0 +1,13 @@
+package ies.carrillo.daw.endes.fibonacci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ fibonacciMain/src/main/java/ies/carrillo/daw/endes/fibonacciMain/fibonaccio_2.java
@@ -0,0 +1,7 @@
+package ies.carrillo.daw.endes.fibonacciMain;
+import ies.carrillo.daw.endes.fibonacci.*;
+public class fibonaccio_2 {
+	public static void main(String[] args) {
+		fibonacci.main(null);
+	}
+}
